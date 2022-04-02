0d00d31095458651769e9448148e66a021242c72
sudo180272 <sudo180272@protonmail.ch>
2022-04-02 09:31:15 +0900

    create venv in python

--- src/python.c
+++ src/python.c
@@ -3,14 +3,16 @@
 #include <string.h>
 
 void createPythonProject(const PythonOptions *options) {
-  char *createVenvCommand = malloc(sizeof(char) * (15 + strlen(options->venvName) + 1)); // 15 is the length of "python -m venv "
+  char *createVenvCommand = malloc(
+      sizeof(char) *
+      (15 + (options->venvName ? strlen(options->venvName) : 4) +
+       1)); // 15 is the length of "python -m venv ", 4 is the length of "venv"
 
   if (options->useVenv) {
     strcpy(createVenvCommand, "python -m venv ");
-    strcat(createVenvCommand, options->venvName);
-    system("python -m venv venv");
+    strcat(createVenvCommand, options->venvName ? options->venvName : "venv");
+    system(createVenvCommand);
   }
 
   free(createVenvCommand);
 }
-
